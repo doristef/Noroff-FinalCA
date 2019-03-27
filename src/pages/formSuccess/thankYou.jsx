fffaa7c7fbda131a40779fe0631e509b37ef62b4
Halldor Valur Stefansson <doristef@gmail.com>
2019-03-27 22:21:00 +0100

    Fixed button

--- src/pages/formSuccess/thankYou.jsx
+++ src/pages/formSuccess/thankYou.jsx
@@ -9,7 +9,7 @@ export default class FormSuccess extends React.Component {
         return (
             <div className="[ row ][ text-center ][ justify-content-center ]">
                 <div className="[ col-sm-12 ][ mt-5 ]">
-                    <div className="[ card card-notFound ]">
+                    <div className="[ card card-shadow card-notFound ]">
                     <div className="[ card-body ]">
                         <h1 className="[ card-title ]">Thank you!</h1>
                         <h2 className="[ card-subtitle ][ mb-2 ][ text-muted ]">Your request was sent.</h2>
@@ -17,8 +17,8 @@ export default class FormSuccess extends React.Component {
                                 Thank you for contacting us.
                             </p>
                         <NavLink className="[ btn btn-color1 btn-lg ][ m-3 ]" exact={true} to="/"> 
-                        <FontAwesomeIcon icon={faHome} size="lg" /> 
-                            Home 
+                        <FontAwesomeIcon icon={faHome} size="lg" className="[ mr-xs-0 mr-md-3 ]" /> 
+                             Go to Home 
                         </NavLink>
                     </div>
                     </div>
