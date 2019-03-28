2e15a41baf9e459db411044be3e594b975af068a
Halldor Valur Stefansson <doristef@gmail.com>
2019-03-28 02:15:00 +0100

    Changed 404 page, added image

--- src/pages/404/notFound.jsx
+++ src/pages/404/notFound.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import NotFound404 from './../../assets/404.png';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faEnvelope } from '@fortawesome/free-solid-svg-icons'
@@ -11,18 +12,20 @@ export default class NotFound extends React.Component {
                 <div className="[ col-sm-12 ][ mt-5 ]">
                     <div className="[ card card-notFound ]">
                     <div className="[ card-body ]">
-                        <h1 className="[ card-title ]">Oooops!</h1>
-                        <h2 className="[ card-subtitle ][ mb-2 ][ text-muted ]">404 Not Found</h2>
+                    
+                    <img src={NotFound404} className="[ img-fluid ][ card-title ][ m-0 p-0 ]" />
+                        
+                        <h2 className="[ card-subtitle ][ mb-2 ][ text-muted ]">Oooooops...</h2>
                             <p className="[ card-text ]">
                                 Sorry, something has gone wrong. <br />
                                 The requested page is nowhere to be found.
                             </p>
                         <NavLink className="[ btn btn-color1 btn-lg ][ m-3 ]" exact={true} to="/"> 
-                            <FontAwesomeIcon icon={faHome} color={iconColor} size="lg" /> 
+                            <FontAwesomeIcon icon={faHome} size="lg" /> 
                             Home 
                         </NavLink>
                         <NavLink className="[ btn btn-color1 btn-lg ][ m-3 ]" exact={true} to="/contact"> 
-                            <FontAwesomeIcon icon={faEnvelope} color={iconColor} size="lg" />  
+                            <FontAwesomeIcon icon={faEnvelope} size="lg" />  
                             Contact Us 
                         </NavLink>
                     </div>
