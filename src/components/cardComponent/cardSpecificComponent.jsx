398dae24d7dd48c64786d0a3e358b50b40e161f7
Halldor Valur Stefansson <doristef@gmail.com>
2019-03-28 02:57:19 +0100

    Updates: Responsive fix

--- src/components/cardComponent/cardSpecificComponent.jsx
+++ src/components/cardComponent/cardSpecificComponent.jsx
@@ -13,15 +13,15 @@ export default class CardSpecificComponent extends React.Component {
                         <div className="[ card card-shadow card-specific ]">
                             <div className="[ card-body ]">
                             <h1 className="[ card-title card-specific-title ][ m-3 ]"> {this.props.name ? this.props.name : 'No name to show.'}</h1>
-                                <div className="[ card-text ][ px-2 ml-sm-0 ml-md-4 mb-5  ]">
+                                <div className="[ card-text ][ px-2 ml-0 ml-md-4 mb-5  ]">
                                     <h5 className="[ mt-2 ][ card-specific-title ]">Rarity:</h5> 
-                                        <span className="[ ml-md-3 ml-sm-0 ]"> {this.props.rarity ? this.props.rarity : 'Nothing to show.'} </span>
+                                        <span className="[ ml-md-3 ml-0 ]"> {this.props.rarity ? this.props.rarity : 'Nothing to show.'} </span>
                                     <h5 className="[ mt-2 ][ card-specific-title ]">Colors:</h5> 
-                                        <span className="[ ml-md-3 ml-sm-0 ]"> {this.props.colors ? this.props.colors : 'Nothing to show.'} </span>
+                                        <span className="[ ml-md-3 ml-0 ]"> {this.props.colors ? this.props.colors : 'Nothing to show.'} </span>
                                     <h5 className="[ mt-2 ][ card-specific-title ]">About:</h5> 
-                                        <span className="[ ml-md-3 ml-sm-0 ]"> {this.props.text ? this.props.text : 'Nothing to show.' } </span>
+                                        <span className="[ ml-md-3 ml-0 ]"> {this.props.text ? this.props.text : 'Nothing to show.' } </span>
                                     <h5 className="[ mt-2 ][ card-specific-title ]">Flavor:</h5> 
-                                        <span className="[ ml-md-3 ml-sm-0 ]"> {this.props.flavor ? this.props.flavor : 'Nothing to show.' } </span>
+                                        <span className="[ ml-md-3 ml-0 ]"> {this.props.flavor ? this.props.flavor : 'Nothing to show.' } </span>
                                 </div>
                             </div>
                         </div>
